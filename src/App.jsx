4588174388074f718cd6aa75e959ee8e93a6584c
Francisco Monleon <paco.monleon@octoenergy.com>
2023-06-27 10:58:55 +0200

    hooks and context

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SocketProvider } from './context/SocketContext'
+import HomePage from './pages/HomePage'
+
+
+const BandNamesApp = () => {
+    return (
+        <SocketProvider>
+            <HomePage />
+        </SocketProvider>
+    )
+}
+
+export default BandNamesApp;
